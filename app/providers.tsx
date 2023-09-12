15c2c01b5b66229c2f42d96d22887f2dcd4c2038
An Nguyen <nguyenan8196@gmail.com>
2023-09-12 22:17:39 +0700

    Refactor

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import * as React from "react";
-import { NextUIProvider } from "@nextui-org/system";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import {NextUIProvider} from "@nextui-org/system";
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+import {ThemeProviderProps} from "next-themes/dist/types";
+import {useSearchParams} from 'next/navigation'
 
 export interface ProvidersProps {
-	children: React.ReactNode;
-	themeProps?: ThemeProviderProps;
+    children: React.ReactNode;
+    themeProps?: ThemeProviderProps;
 }
 
-export function Providers({ children, themeProps }: ProvidersProps) {
-	return (
-		<NextUIProvider>
-			<NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-		</NextUIProvider>
-	);
+export function Providers({children, themeProps}: ProvidersProps) {
+    const searchParams = useSearchParams();
+    const code = searchParams.get('code');
+
+    return (
+        <NextUIProvider>
+            <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+        </NextUIProvider>
+    );
 }
